f4a6683c99eb33fa4890255cc37ff66ca12de6a3
matgor <mateusz.gorczyca@gmail.com>
2017-12-02 11:09:01 +0100

    added google map, js file

---
+++ script.js
@@ -0,0 +1,12 @@
+function initMap() {
+    var uluru = {lat: 54.348024, lng: 18.664599};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        animation: google.maps.Animation.BOUNCE
+    });
+}
